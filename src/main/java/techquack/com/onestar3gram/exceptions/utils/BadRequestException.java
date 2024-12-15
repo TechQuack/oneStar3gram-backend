725b3d6ba8f93f62da91b13a764c5298792b4da9
izana <izana.khabouri@univ-rouen.fr>
2024-12-15 08:50:13 +0100

    feat: add exception to manage bad request exception

---
+++ src/main/java/techquack/com/onestar3gram/exceptions/utils/BadRequestException.java
@@ -0,0 +1,8 @@
+package techquack.com.onestar3gram.exceptions.utils;
+
+public class BadRequestException extends RuntimeException {
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/techquack/com/onestar3gram/exceptions/utils/UtilsExceptionHandler.java
@@ -0,0 +1,28 @@
+package techquack.com.onestar3gram.exceptions.utils;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import techquack.com.onestar3gram.entities.ErrorEntity;
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class UtilsExceptionHandler {
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ErrorEntity> badRequestHandler(BadRequestException exception) {
+        ErrorEntity error = new ErrorEntity(LocalDateTime.now());
+        error.setHttpStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exception.getMessage());
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(error);
+    }
+
+    @ExceptionHandler(EmptyException.class)
+    public ResponseEntity<ErrorEntity> noContentHandler(EmptyException exception) {
+        ErrorEntity error = new ErrorEntity(LocalDateTime.now());
+        error.setHttpStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exception.getMessage());
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(error);
+    }
+}
