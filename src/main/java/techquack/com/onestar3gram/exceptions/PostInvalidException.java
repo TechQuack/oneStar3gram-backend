6f27bbfff2618c93a55edb29311e7baafcf751c1
izana <izana.khabouri@univ-rouen.fr>
2024-12-15 08:49:18 +0100

    feat: add exception to manage post exception

--- src/main/java/techquack/com/onestar3gram/exceptions/PostInvalidException.java
+++
@@ -1,4 +0,0 @@
-package techquack.com.onestar3gram.exceptions;
-
-public class PostInvalidException extends Exception {
-}
---
+++ src/main/java/techquack/com/onestar3gram/exceptions/post/PostExceptionHandler.java
@@ -0,0 +1,20 @@
+package techquack.com.onestar3gram.exceptions.post;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import techquack.com.onestar3gram.entities.ErrorEntity;
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class PostExceptionHandler {
+
+    @ExceptionHandler(PostInvalidException.class)
+    public ResponseEntity<ErrorEntity> postNotFoundHandler(PostInvalidException exception) {
+        ErrorEntity error = new ErrorEntity(LocalDateTime.now());
+        error.setHttpStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exception.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(error);
+    }
+}
