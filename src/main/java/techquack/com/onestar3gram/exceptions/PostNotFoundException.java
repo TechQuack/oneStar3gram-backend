eaf7d4ca08cb3e308718da260feefbbcd0127ec9
Hortense Cauchois <hortensecauchois@gmail.com>
2024-12-15 21:51:27 +0100

    feat: add PostNotFoundException

---
+++ src/main/java/techquack/com/onestar3gram/exceptions/PostNotFoundException.java
@@ -0,0 +1,7 @@
+package techquack.com.onestar3gram.exceptions;
+
+public class PostNotFoundException extends Exception {
+    public PostNotFoundException(String message) {
+        super(message);
+    }
+}
