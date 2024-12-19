73c4f04adaeb3130159e937b3fbf819cbbe10a17
vanliflo <florian.van-liedekerke@univ-rouen.fr>
2024-12-19 22:57:30 +0100

    chore: delete AppUser

--- src/main/java/techquack/com/onestar3gram/exceptions/UsernameAlreadyTakenException.java
+++
@@ -1,5 +0,0 @@
-package techquack.com.onestar3gram.exceptions;
-
-public class UsernameAlreadyTakenException extends Exception {
-    public UsernameAlreadyTakenException(String username) { super("User not found with username : " + username);}
-}
